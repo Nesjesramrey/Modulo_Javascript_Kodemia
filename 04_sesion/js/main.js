df9e063e3ca34f547bebcd2cc85656c3d0cfc6a6
Nestor <nesjesramrey2@gmail.com>
2022-04-08 22:08:13 -0500

    add exercises 1y2

--- 04_sesion/js/main.js
+++ 04_sesion/js/main.js
@@ -1,1 +1,38 @@
-alert("sesion strings")
+/*
+Ejercicio 1:
+    Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre
+*/
+
+// enter the complete
+let myName = prompt("Ingresa tu nombre completo");
+// delate spaces
+let namewhitoutspaces = myName.replace(/\s+/g, '')
+
+
+// count the letters
+function countLetters(name){
+// return number of letters
+    return (name.length)
+}
+
+//call the function
+console.log("Tu nombre tiene "+countLetters(namewhitoutspaces)+ " letras")
+
+
+
+/*
+Ejercicio 2:
+    Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene.
+*/
+
+function countVowel(vowels) { 
+    //  count of vowels
+    const count = vowels.match(/[aeiou]/gi).length;
+    // return number of vowels
+    return count;
+}
+
+//call the funtion
+console.log("Tu nombre tiene "+ countVowel(myName)+ " vocales")
+
+
