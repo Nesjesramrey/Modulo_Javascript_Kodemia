a411d5a7a4a04941bc3905ee10ff07b50b626e3f
Nestor <nesjesramrey2@gmail.com>
2022-04-13 17:53:05 -0500

    add solution

--- 06_sesion/js/main.js
+++ 06_sesion/js/main.js
@@ -12,7 +12,6 @@
 
 // console.log(personas[0]) //La idea es que cada que vez que quisieramos imprimir un valor del array no tengamos que hacerlo uno por unos
 
-
 //Funcion while (primero pregunta la condicion y despues el cuerpo)
 
 // function cicloWhile(num){
@@ -21,19 +20,18 @@
 
 //     while(index < num){   //while index is smaller than five
 //         index++;   // retunn index plus 1
-    
-//                 console.log(index)  //personas[print] console.log(index)  //print 
+
+//                 console.log(index)  //personas[print] console.log(index)  //print
 //         console.log('ciclo activado')
 //     }
 // }
 
 // cicloWhile(5);
 
-
 //Ciclo Do While
 // function cicloDoWhile(){
 
-//     let index = 0 
+//     let index = 0
 
 //     do{
 //         ++index
@@ -42,13 +40,11 @@
 // }
 // cicloDoWhile()
 
-
 //Ciclo for
 
-// for (let index = 0; index < personas.length; index++){    
+// for (let index = 0; index < personas.length; index++){
 //     console.log(personas[index])
 // }
-
 
 // let palabra = prompt('Inserta una palabra');
 
@@ -68,7 +64,6 @@
 
 // convertirPalabra(palabra);
 
-
 // Ejercicio 1:
 // Solicitar al usuario ingresar un numero
 // Crear una funcion que genere un string con caracteres aleatorios,
@@ -76,13 +71,24 @@
 // input : 6
 // output: ‘6AQX>H’
 
-// let number = prompt("Ingresa un numero")
+let number1 = prompt("Ingresa un numero");
 
-// function numberRandom (number1){
-//     while(number>number1)
-// }
+function numberRandom(randomNumber) {
+  let result2 = "";
+  for (let index = 1; index <= randomNumber; index++) {
+    
+    let Myarray = index;
+    result2 += Myarray
+    result3 = String.fromCharCode(result2)
+     
+     
+  }
+  console.log(result2);
+  console.log(result3);
+  
+}
 
-
+numberRandom(number1)
 
 // Ejercicio 2:
 // Pedir al usuario usuario un numero entre 1 y 10
@@ -91,12 +97,12 @@
 // 3 x 1 = 3
 // 3 x 2 = 6
 
-let numberOne = Number(prompt("Ingresa un numero"))
+let numberOne = Number(prompt("Ingresa un numero"));
 
-function tablas(number2){
-for (let index = 0; index < 10; index++){ 
-    console.log(numberOne + "x" + index + "="+ number2*index++)
-   }
+function tablas(number2) {
+  for (let index = 1; index <= 10; index++) {
+    console.log(numberOne + " x " + index + " = " + number2 * index); //aqui habia colocado  index++ me generaba un error ya que saltaba por 2, esto ya que primero sumaba 1 en el for y despues en el console.log
+  }
 }
 
-tablas(numberOne)
+tablas(numberOne);
