def001324d37c9e976e11b6fba9b2157eb8e7e92
Nestor <nesjesramrey2@gmail.com>
2022-04-20 18:36:48 -0500

    modified sesion8

--- 08_sesion/js/sesion08.js
+++ 08_sesion/js/sesion08.js
@@ -125,39 +125,150 @@ function FiltrarPorKoder (){
 
 
 
-// let nombres = []
-// const saludar = (name) => {
-//     nombres.push(name)
+// filtrarPorMateria()
+
+let nombres = []
+const saludar = (name) => {
+  nombres.push(name)
+};
+
+saludar('ivan')
+
+
+
+
+const filtrarPorMateria = (materia) => {
+    let result = '';
+
+    for (let index = 0; index < generacion18.length; index++) {
+      if(materia === generacion18[index].signature) {
+         result = generacion18[index];
+      }
+    }
+
+    return result;
+};
+
+console.log(filtrarPorMateria('Js'))
+
+
+// un callback es una function que es enviada como parametro a otra funcion
+// hight order function es una funcion que recibe una funcion como parametro
+
+
+// function cocinarPan(algo) {
+//   console.log(algo)
+// };
+
+// function prepararCarne() {
+//   console.log('carne preparada')
+
+// }
+// function picarTomate() {
+//   console.log('tomate picado')
 // }
 
-// saludar("ivan")
-// console.log(nombres)
+
+// function armarHamburguesa (cocinarPan, cocinarCarne, picarTomate) {
+//   let panPreparado = true
+
+//   cocinarPan(panPreparado)
+// };
+
+
+// armarHamburguesa(panPreparado, carnePreparada);
+
+
+
+
+// for each 
+// Ejecuta el callback por cada elemento del arreglo 
+
+const numeros = [ 2, 4, 5];
+
+let resultado = 0
+
+numeros.forEach(( item, index, arr ) => console.log(index))
+
+console.log(resultado)
 
 
 
 
 
 
-//console.log(generacion18[0])
+//  parametros nombrados
+function informacionPersona({ nombre, apellido, curp, rfc, fechaNacimiento, direccion, edad, sexo }){
+
+  return {
+    nombre: nombre,
+    apellido: apellido,
+    curp: curp,
+    rfc: rfc,
+    fechaNacimiento: fechaNacimiento,
+    direccion: direccion,
+    edad: edad,
+    sexo: sexo,
+  }
+
+}
+
+informacionPersona({ curp: 'DIAI989s', nombre:'ivan', fechaNacimiento: 'ter' })
 
 
-// generacion18.forEach(object =>{
-//     if(object.signature === "Js"){
-//         console.log(object.signature, object.koders, object.mentor);
-//     }
-// });
-    
+// destructuring 
 
-// const filtrarPorMateria = (materia) =>{
-//     let result ='';
+//Ejercicio 1
 
-//     for(let index = 0; index < generacion18.length; index++){
-//         if(materia === generacion18[index].signature){
-//             result = generacion18[index];
-//         }
-//     }
-//     return result;
+// input 
+// [
+//   'Jason Vorhees',
+//   'Penny Wise',
+//   'Freddy Krueger',
+// ]
+
+// output: 
+// [
+//   'koder 1: Jason Vorhees'
+//   'koder 2: Penny Wise',
+//   'koder 3: Freddy Krueger',
+// ]
+
+
+
+// input:
+// [
+//   {
+//     koder: 'Arnold',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Gerald',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Gelga',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Peer',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Thomas',
+//     score: 8,
+//   }
+// ];
+
+
+// <60 F
+// <70 D
+// <80 C
+// <90 B
+// <100 A
+
+
+// output: {
+//   promedio del grupo: 90
+//   calificacion del grupo: A
 // }
-
-// console.log(filtrarPorMateria('Js'))
-
