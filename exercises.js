f84e778c41b9ee48a6f63d9def0c28bf4eae4ac9
Nestor <nesjesramrey2@gmail.com>
2022-05-02 16:04:50 -0500

    add excersices js

--- exercises.js
+++ exercises.js
@@ -12,19 +12,56 @@
 
 //**Exercise2 */
 
-//find sintax error in 
-//  function multi (a,b) { 
+//find sintax error in
+//  function multi (a,b) {
 //      a*b}
 // console.log(multi(3,2))
 
+//** */
 
-    //  You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//  You probably know the "like" system from Facebook and other pages.
+// People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
-    //  Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
-     
-    //  []                                -->  "no one likes this"
-    //  ["Peter"]                         -->  "Peter likes this"
-    //  ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-    //  ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-    //  ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
-    //  Note: For 4 or more names, the number in "and 2 others" simply increases.     
+//  Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+//  []                                -->  "no one likes this"
+//  ["Peter"]                         -->  "Peter likes this"
+//  ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+//  ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+//  ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+//  Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+// let array1 = [];
+// let array2 = ["Peter"];
+// let array3 = ["Jacob", "Alex"];
+// let array4 = ["Max", "John", "Mark"];
+// let array5 = ["Alex", "Jacob", "Mark", "Max"];
+// const likes = (array) => {
+//   if (array.length === 0) return console.log("'no one likes this'");
+//   else if (array.length <= 1) return console.log(`'${array[0]} likes this'`);
+//   else if (array.length <= 2)
+//     return console.log(`'${array[0]} and ${array[1]} likes this'`);
+//   else if (array.length <= 3)
+//     return console.log(`'${array[0]}, ${array[1]} and ${array[2]} likes this'`);
+// else{
+//     return console.log(`'${array[0]}, ${array[1]} and ${array.length-2} others likes this'`);
+// }
+
+// };
+// likes(array5);
+
+
+//** excercise 3 */
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+
+
