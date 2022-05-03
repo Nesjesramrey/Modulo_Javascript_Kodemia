caeeea3bfee283980f0af6538c2356cf497ebb15
Nestor <nesjesramrey2@gmail.com>
2022-05-02 23:23:48 -0500

    exercise add class

--- 16_sesion/js/sesion16_HW.js
+++ 16_sesion/js/sesion16_HW.js
@@ -2,228 +2,241 @@
 
 const mentorsArray = [
   {
-      name:"Ivan Diaz Alarcon",
-      scores:[
-          {
-              signature:"HTML",
-              score:8
-          },
-          {
-              signature:"CSS",
-              score:10
-          },
-          {
-              signature:"JS",
-              score:8
-          },
-          {
-              signature:"ReactJS",
-              score:8
-          }
-      ]
+    name: "Ivan Diaz Alarcon",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8,
+      },
+      {
+        signature: "CSS",
+        score: 10,
+      },
+      {
+        signature: "JS",
+        score: 8,
+      },
+      {
+        signature: "ReactJS",
+        score: 8,
+      },
+    ],
   },
   {
-      name:"Alejandra Saenz Gonzalez",
-      scores:[
-          {
-              signature:"HTML",
-              score:8
-          },
-          {
-              signature:"CSS",
-              score:7
-          },
-          {
-              signature:"JS",
-              score:10
-          },
-          {
-              signature:"ReactJS",
-              score:10
-          }
-      ]
+    name: "Alejandra Saenz Gonzalez",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8,
+      },
+      {
+        signature: "CSS",
+        score: 7,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+      {
+        signature: "ReactJS",
+        score: 10,
+      },
+    ],
   },
   {
-      name:"Alan Hernandex Hernandex",
-      scores:[
-          {
-              signature:"HTML",
-              score:9
-          },
-          {
-              signature:"CSS",
-              score:9
-          },
-          {
-              signature:"JS",
-              score:10
-          },
-          {
-              signature:"ReactJS",
-              score:9
-          }
-      ]
+    name: "Alan Hernandex Hernandex",
+    scores: [
+      {
+        signature: "HTML",
+        score: 9,
+      },
+      {
+        signature: "CSS",
+        score: 9,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+      {
+        signature: "ReactJS",
+        score: 9,
+      },
+    ],
   },
   {
-      name:"Raul Aguirre Salazar",
-      scores:[
-          {
-              signature:"HTML",
-              score:8
-          },
-          {
-              signature:"CSS",
-              score:10
-          },
-          {
-              signature:"JS",
-              score:9
-          },
-          {
-              signature:"ReactJS",
-              score:10
-          }
-      ]
+    name: "Raul Aguirre Salazar",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8,
+      },
+      {
+        signature: "CSS",
+        score: 10,
+      },
+      {
+        signature: "JS",
+        score: 9,
+      },
+      {
+        signature: "ReactJS",
+        score: 10,
+      },
+    ],
   },
   {
-    name:"Pepito Potter Ron",
-    scores:[
-        {
-            signature:"HTML",
-            score:8
-        },
-        {
-            signature:"CSS",
-            score:10
-        },
-        {
-            signature:"JS",
-            score:9
-        },
-        {
-            signature:"ReactJS",
-            score:10
-        }
-    ]
-}
-]
+    name: "Pepito Potter Ron",
+    scores: [
+      {
+        signature: "HTML",
+        score: 8,
+      },
+      {
+        signature: "CSS",
+        score: 10,
+      },
+      {
+        signature: "JS",
+        score: 9,
+      },
+      {
+        signature: "ReactJS",
+        score: 10,
+      },
+    ],
+  },
+];
 
 // const tr  = document.createElement('tr');
 
 // const createNode = (name) => {
-      
+
 //     const td = document.createElement('td');
 //     const koderName = document.createTextNode(name);
 //     td.appendChild(koderName);
-//     tr.appendChild(td)    
+//     tr.appendChild(td)
 //     document.body.appendChild(tr)
 // }
-
 
 //** Mi solucion */
 // const table = document.createElement('table');
 
 // mentorsArray.forEach((item) =>{
 //     //createNode(item.name)
-//     const tr  = document.createElement('tr');    
+//     const tr  = document.createElement('tr');
 //     const createNode = (name) => {
-      
+
 //         const td = document.createElement('td');
 //         const koderName = document.createTextNode(name);
 //         td.appendChild(koderName);
 //         tr.appendChild(td)
-//         table.appendChild(tr)    
+//         table.appendChild(tr)
 //         document.body.appendChild(table)
 //     }
 //     createNode(item.name)
-    
+
 //     item.scores.forEach((score) =>{
 //         if(item.name === item.name) {
-        
+
 //         const createNode2 = (name) => {
-      
+
 //             const td = document.createElement('td');
 //             const koderName = document.createTextNode(name);
 //             td.appendChild(koderName);
-//             tr.appendChild(td)    
-            
+//             tr.appendChild(td)
+
 //         }
-        
+
 //         createNode2(score.score)
-//     }         
-//     })  
+//     }
+//     })
 // })
 
-
 const creartTable = () => {
-    const table = document.createElement('table')
-    const thead = document.createElement('thead')
-    const tbody = document.createElement('tbody')
-    const trHead = document.createElement('tr')
-    
-    const thName = document.createElement('th')
-    const thHTML = document.createElement('th')
-    const thCSS = document.createElement('th')
-    const thJS = document.createElement('th')
-    const thReactJS = document.createElement('th')
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
+  const trHead = document.createElement("tr");
 
-    const textName =document.createTextNode('Name')
-    const textHTML =document.createTextNode('HTML')
-    const textCSS =document.createTextNode('CSS')
-    const textJS =document.createTextNode('JS')
-    const textReactJs =document.createTextNode('ReactJs')
+  const thName = document.createElement("th");
+  const thHTML = document.createElement("th");
+  const thCSS = document.createElement("th");
+  const thJS = document.createElement("th");
+  const thReactJS = document.createElement("th");
+  const thPromedio = document.createElement("th");
 
-    table.appendChild(thead)
-    table.appendChild(tbody)
-    thead.appendChild(trHead)
-    
+  const textName = document.createTextNode("Mentor Name");
+  const textHTML = document.createTextNode("HTML");
+  const textCSS = document.createTextNode("CSS");
+  const textJS = document.createTextNode("JS");
+  const textReactJs = document.createTextNode("ReactJs");
+  const textPromedio = document.createTextNode("Promedio");
 
-    trHead.appendChild(thName)
-    trHead.appendChild(thHTML)
-    trHead.appendChild(thCSS)    
-    trHead.appendChild(thJS)
-    trHead.appendChild(thReactJS)
-    
-    thName.appendChild(textName)
-    thHTML.appendChild(textHTML)
-    thCSS.appendChild(textCSS)
-    thJS.appendChild(textJS)
-    thReactJS.appendChild(textReactJs)
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  thead.appendChild(trHead);
 
-    document.body.appendChild(table)
+  trHead.appendChild(thName);
+  trHead.appendChild(thHTML);
+  trHead.appendChild(thCSS);
+  trHead.appendChild(thJS);
+  trHead.appendChild(thReactJS);
+  trHead.appendChild(thPromedio);
 
+  thName.appendChild(textName);
+  thHTML.appendChild(textHTML);
+  thCSS.appendChild(textCSS);
+  thJS.appendChild(textJS);
+  thReactJS.appendChild(textReactJs);
+  thPromedio.appendChild(textPromedio);
 
-mentorsArray.forEach((item) =>{
+  document.body.appendChild(table);
+
+  mentorsArray.forEach((item) => {
     //createNode(item.name)
-    const tr  = document.createElement('tr');    
+    const tr = document.createElement("tr");
     const createNode = (name) => {
-      
-        const td = document.createElement('td');
-        const koderName = document.createTextNode(name);
-        td.appendChild(koderName);
-        tr.appendChild(td)
-        table.appendChild(tr)    
-        document.body.appendChild(table)
-    }
-    createNode(item.name)
-    
-    item.scores.forEach((score) =>{
-        if(item.name === item.name) {
-        
+      const td = document.createElement("td");
+      const koderName = document.createTextNode(name);
+      td.appendChild(koderName);
+      tr.appendChild(td);
+      table.appendChild(tr);
+      document.body.appendChild(table);
+    };
+    createNode(item.name);
+
+    item.scores.forEach((score) => {
+      {
         const createNode2 = (name) => {
-      
-            const td = document.createElement('td');
-            const koderName = document.createTextNode(name);
-            td.appendChild(koderName);
-            tr.appendChild(td)    
-            
-        }
-        
-        createNode2(score.score)
-    }         
-    })  
-})
+          const tdSingleScore = document.createElement("td");
+          const koderScore = document.createTextNode(name);
+          tdSingleScore.appendChild(koderScore);
+          tr.appendChild(tdSingleScore);
+        };
+        createNode2(score.score);
+      }
+    });
+    const tdAvgScore = document.createElement("td");
+    let totalMentor = 0;
+    let numSignature = item.scores.length
+    item.scores.forEach((score) => {
+     totalMentor += score.score;     
+    })
+    let avgMentor = totalMentor/ numSignature;
+    const koderAvgScore = document.createTextNode(avgMentor);
+    tdAvgScore.appendChild(koderAvgScore);
+    tr.appendChild(tdAvgScore);
+    if(avgMentor >9){
+        tdAvgScore.classList.add('green');
+    }
+    if(avgMentor <9 && avgMentor>6){
+        tdAvgScore.classList.add('orange');
+    } 
+    if(avgMentor<6){
+        tdAvgScore.classList.add('red');
+    }    
+  });
+};
 
-}
-
-creartTable()
+creartTable();
